79ca820cc091d8dbff2d178f2008982ca8023abe
GuinaCosta <a.costa13@hotmail.com>
2016-02-28 23:03:13 -0300

    HTTP, directives e controllers

--- script.js
+++ script.js
@@ -16,4 +16,31 @@
         $scope.person = person;
     }]);
     
+    module.controller("HttpController", ['$scope', '$http', function($scope, $http){
+        var authCode = '?client_id=guinacosta&client_secret=coxas123ha';
+        
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+                    
+            $http.get($scope.user.repos_url + authCode)
+                .then(onRepos, onError);
+
+        }
+        
+        var onError = function (reason) {
+            $scope.errorMessage = "Deu ruim pra buscar os dados... :(";
+        }
+        
+        var onRepos = function(response){
+            $scope.respositorios = response.data;
+        }
+        
+        $scope.buscar = function(username) {
+            $http.get("https://api.github.com/users/" + username + authCode)
+                .then(onUserComplete, onError);    
+        }
+        
+        $scope.repoSortOrder = "+language";
+    }]);
+    
 }(window.angular));
